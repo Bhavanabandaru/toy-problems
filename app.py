c80c2a3c9840376a417ece3309f44bb553940104
Bhavana B <38123470+Bhavanabandaru@users.noreply.github.com>
2021-04-12 11:13:06 +0530

    Reegistration done

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
-  
-@app.route('/')
-def index():
-    return '<h1>Hello</h1>'
+from flask import Flask , render_template, request
 
-@app.route("/bhavana")
-def bhavana():
-   return "Hello, Bhavana!!"
+app = Flask(__name__)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # headline = "uname has registered"
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+
+        # return redirect(url_for("login"))
+    return render_template("index.html")
+
+
+
+# @app.route('/register',methods=["GET","POST"])
+# def register():
+#     msg = ''
+#     if request.method =="GET":
+#         msg = "You have successfully registered."
+
+#     return render_template("index.html")
+
+# @app.route("/bhavana")
+# def bhavana():
+#    return "Hello, Bhavana!!"
+
